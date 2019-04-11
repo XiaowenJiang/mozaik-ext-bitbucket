a4ad12cf0931478bd64997888898398868b10eb6
Julie Jiang <Julie.Jiang@emc.com>
2019-04-11 08:15:10 +0000

    Add pull request author to widget

--- src/components/PullRequest.jsx
+++ src/components/PullRequest.jsx
@@ -5,10 +5,14 @@ class PullRequest extends Component {
     render() {
         const { pullRequest } = this.props;
         const addr = pullRequest.links.self[0].href;
+        const author = pullRequest.author.user.displayName;
         return (
             <div className="list__item bitbucket__pull-request">
                 <span className="bitbucket__pull-request__id">
                     { pullRequest.id }
+                </span>
+                <span className="bitbucket__pull-request__author">
+                    { author }
                 </span>
                 <a href={addr} className="bitbucket__pull-request__title">
                     { pullRequest.title }
